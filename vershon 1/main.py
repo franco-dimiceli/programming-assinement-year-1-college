69b8afb5534fb03456bcbf41675b8fe4183ae391
franco-dimiceli <120249248+franco-dimiceli@users.noreply.github.com>
2023-02-28 16:17:48 +0000

    update

--- vershon 1/main.py
+++ vershon 1/main.py
@@ -1,55 +1,100 @@
+
 ## importing tkinter ##
 
 import tkinter as tk
 
-from tkinter import messagebox
+class my_gui:
 
-## initialising the gui ##
+##    ##----------------------------------------------------------------------------------------------------------------
 
-root = tk.Tk()
+    ## functions ##
 
-root.title("scoring system")
+    def switch_to_events_menu_frame(self):
 
-root.geometry("900x800")
+        self.event_menu_frame_navigation.pack(fill="x", )
+
+        self.navigation_button_frame.pack_forget()
+
+    def switch_to_start_menu_frame(self):
+
+        self.navigation_button_frame.pack(fill="x", )
+
+        self.event_menu_frame_navigation.pack_forget()
+
+    def __init__(self):
+
+        ## initialising the gui ##
+
+        self.root = tk.Tk()
+
+        self.root.title("scoring system")
+
+        self.root.geometry("900x800")
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+        ## frames ##
 
 
-## functions ##
+        # self.navigation_bar_frame = tk.Frame(self.root, bg="red")
+        #
+        # self.navigation_bar_frame.configure(self.navigation_bar_frame, height=300, )
+        #
+        # self.navigation_bar_frame.pack()
 
-def warning_box():
+        self.navigation_button_frame = tk.Frame(self.root, )
 
-    if messagebox.askyesno(title="Quit", message="are you sure"):
+        self.navigation_button_frame.columnconfigure(0, weight=1, )
+        self.navigation_button_frame.columnconfigure(1, weight=1, )
+        self.navigation_button_frame.columnconfigure(2, weight=1, )
+        self.navigation_button_frame.columnconfigure(3, weight=1, )
+        self.navigation_button_frame.columnconfigure(4, weight=1, )
 
-        root.destroy()
+        self.display_scoreboard_button = tk.Button(self.navigation_button_frame, text="display scoreboard", font=("", 18), height=2, bg="blue")
+        self.display_scoreboard_button.grid(row=0, column=0, sticky=tk.W + tk.E)
+
+        self.edit_teams_and_individuals_button = tk.Button(self.navigation_button_frame, text="edit teams and individuals", font=("", 18), height=2, )
+        self.edit_teams_and_individuals_button.grid(row=0, column=1, sticky=tk.W + tk.E)
+
+        self.start_menu_button = tk.Button(self.navigation_button_frame, text="start menu", font=("", 18), height=2, command=self.switch_to_start_menu_frame)
+        self.start_menu_button.grid(row=0, column=2, sticky=tk.W + tk.E)
+
+        self.event_menu_button = tk.Button(self.navigation_button_frame, text= "edit events", font=("", 18), height=2, command=self.switch_to_events_menu_frame)
+        self.event_menu_button.grid(row=0, column=3, sticky=tk.W + tk.E)
+
+        self.close_program_button = tk.Button(self.navigation_button_frame, text="close program", font=("", 18), height=2, )
+        self.close_program_button.grid(row=0, column=4, sticky=tk.W + tk.E)
+
+        self.navigation_button_frame.pack(fill="x", )
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+        ## events menu navigation ##
+
+        self.event_menu_frame_navigation = tk.Frame(self.root, )
+
+        self.event_menu_frame_navigation.columnconfigure(0, weight=1, )
+        self.event_menu_frame_navigation.columnconfigure(1, weight=1, )
+        self.event_menu_frame_navigation.columnconfigure(2, weight=1, )
+        self.event_menu_frame_navigation.columnconfigure(3, weight=1, )
+        self.event_menu_frame_navigation.columnconfigure(4, weight=1, )
+
+        self.display_scoreboard_button = tk.Button(self.event_menu_frame_navigation, text="display scoreboard", font=("", 18), height=2, )
+        self.display_scoreboard_button.grid(row=0, column=0, sticky=tk.W + tk.E)
+
+        self.edit_teams_and_individuals_button = tk.Button(self.event_menu_frame_navigation, text="edit teams and individuals", font=("", 18), height=2, )
+        self.edit_teams_and_individuals_button.grid(row=0, column=1, sticky=tk.W + tk.E)
+
+        self.start_menu_button = tk.Button(self.event_menu_frame_navigation, text="start menu", font=("", 18), height=2, command=self.switch_to_start_menu_frame)
+        self.start_menu_button.grid(row=0, column=2, sticky=tk.W + tk.E)
+
+        self.event_menu_button = tk.Button(self.event_menu_frame_navigation, text="edit events", font=("", 18), height=2, command=self.switch_to_events_menu_frame)
+        self.event_menu_button.grid(row=0, column=3, sticky=tk.W + tk.E)
+
+        self.back_button = tk.Button(self.event_menu_frame_navigation, text="back", font=("", 18), height=2, )
+        self.back_button.grid(row=0, column=4, sticky=tk.W + tk.E)
+
+        self.root.mainloop()
 
 
-# def warning_box_close_program():
-
-## navigation buttons ##
-
-navigation_button_frame = tk.Frame(root, )
-
-navigation_button_frame.columnconfigure(0, weight=1, )
-navigation_button_frame.columnconfigure(1, weight=1, )
-navigation_button_frame.columnconfigure(2, weight=1, )
-navigation_button_frame.columnconfigure(3, weight=1, )
-
-display_scoreboard_button = tk.Button(navigation_button_frame, text="display scoreboard", font=("", 18), height=2, )
-display_scoreboard_button.grid(row=0, column=0, sticky=tk.W + tk.E)
-
-edit_teams_and_individuals_button = tk.Button(navigation_button_frame, text="edit teams and individuals", font=("", 18),
-                                              height=2, )
-edit_teams_and_individuals_button.grid(row=0, column=1, sticky=tk.W + tk.E)
-
-start_menu_button = tk.Button(navigation_button_frame, text="start menu", font=("", 18), height=2, )
-start_menu_button.grid(row=0, column=2, sticky=tk.W + tk.E)
-
-close_program_button = tk.Button(navigation_button_frame, text="close program", font=("", 18), height=2, )
-close_program_button.grid(row=0, column=3, sticky=tk.W + tk.E)
-
-root.protocol("WM_DELETE_WINDOW", warning_box())
-
-navigation_button_frame.pack(fill="x", )
-
-## edit events menu ##
-
-root.mainloop()
+my_gui()
