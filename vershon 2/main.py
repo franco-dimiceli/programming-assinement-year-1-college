7f7a866b337a31fc6c2978ee230fbd67dba0dac6
franco-dimiceli <120249248+franco-dimiceli@users.noreply.github.com>
2023-03-01 19:57:47 +0000

    update

--- vershon 2/main.py
+++ vershon 2/main.py
@@ -3,6 +3,8 @@
 ## importing custom tkinter ##
 
 import customtkinter
+import tkinter
+from tkinter import messagebox
 
 class my_gui:
 
@@ -10,19 +12,43 @@ class my_gui:
 
     ## functions ##
 
+## switching navigation frame functions ##
+
     def switch_to_events_menu_frame(self):
 
         self.event_menu_frame_navigation.pack(fill="x", )
 
         self.navigation_button_frame.pack_forget()
+        self.teams_and_individuals_frame_navigation.pack_forget()
+        self.display_scoreboard_frame_navigation.pack_forget()
 
     def switch_to_start_menu_frame(self):
 
         self.navigation_button_frame.pack(fill="x", )
 
         self.event_menu_frame_navigation.pack_forget()
+        self.teams_and_individuals_frame_navigation.pack_forget()
+        self.display_scoreboard_frame_navigation.pack_forget()
+
+    def switch_to_edit_teams_and_individuals_frame(self):
+
+        self.teams_and_individuals_frame_navigation.pack(fill="x", )
+
+        self.navigation_button_frame.pack_forget()
+        self.event_menu_frame_navigation.pack_forget()
+        self.display_scoreboard_frame_navigation.pack_forget()
+
+    def switch_to_display_scoreboard_frame(self):
+
+        self.display_scoreboard_frame_navigation.pack(fill="x", )
+
+        self.navigation_button_frame.pack_forget()
+        self.event_menu_frame_navigation.pack_forget()
+        self.teams_and_individuals_frame_navigation.pack_forget()
 
     def __init__(self):
+
+    ## initialising the GUI ##
 
         self.root = customtkinter.CTk()
 
@@ -30,11 +56,17 @@ class my_gui:
 
         self.root.geometry("900x800")
 
-##        ##------------------------------------------------------------------------------------------------------------
+        customtkinter.set_appearance_mode("dark")
+
+        customtkinter.set_default_color_theme("dark-blue")
 
     ## frames ##
 
-        self.navigation_button_frame = customtkinter.CTkFrame(self.root)
+##        ##------------------------------------------------------------------------------------------------------------
+
+    ## start menu navigation buttons ##
+
+        self.navigation_button_frame = customtkinter.CTkFrame(self.root, )
 
         self.navigation_button_frame.columnconfigure(0, weight=1, )
         self.navigation_button_frame.columnconfigure(1, weight=1, )
@@ -42,17 +74,21 @@ class my_gui:
         self.navigation_button_frame.columnconfigure(3, weight=1, )
         self.navigation_button_frame.columnconfigure(4, weight=1, )
 
-        self.display_scoreboard_button = customtkinter.CTkButton(self.navigation_button_frame, text="display scoreboard", font=("", 18), height=2, ).grid(row=0, column=0, sticky=customtkinter.W + customtkinter.E)
+        self.display_scoreboard_button = customtkinter.CTkButton(self.navigation_button_frame, text="\nDisplay scoreboard\n", font=("roboto", 20), height=0, command=self.switch_to_display_scoreboard_frame, ).grid(row=0, column=0, sticky=customtkinter.W + customtkinter.E,)
 
-        self.edit_teams_and_individuals_button = customtkinter.CTkButton(self.navigation_button_frame, text="edit teams and individuals", font=("", 18), height=2, ).grid(row=0, column=1, sticky=customtkinter.W + customtkinter.E)
+        self.edit_teams_and_individuals_button = customtkinter.CTkButton(self.navigation_button_frame, text="\nEdit teams and individuals\n", font=("roboto", 20), height=0, command=self.switch_to_edit_teams_and_individuals_frame).grid(row=0, column=1, sticky=customtkinter.W + customtkinter.E)
 
-        self.start_menu_button = customtkinter.CTkButton(self.navigation_button_frame, text="start menu", font=("", 18), height=2, command=self.switch_to_start_menu_frame).grid(row=0, column=2, sticky=customtkinter.W + customtkinter.E)
+        self.start_menu_button = customtkinter.CTkButton(self.navigation_button_frame, text="\nStart menu\n", font=("roboto", 20), height=0, command=self.switch_to_start_menu_frame).grid(row=0, column=2, sticky=customtkinter.W + customtkinter.E)
 
-        self.event_menu_button = customtkinter.CTkButton(self.navigation_button_frame, text="edit events", font=("", 18), height=2, command=self.switch_to_events_menu_frame).grid(row=0, column=3, sticky=customtkinter.W + customtkinter.E)
+        self.event_menu_button = customtkinter.CTkButton(self.navigation_button_frame, text="\nEdit events\n", font=("roboto", 20), height=0, command=self.switch_to_events_menu_frame).grid(row=0, column=3, sticky=customtkinter.W + customtkinter.E)
 
-        self.close_program_button = customtkinter.CTkButton(self.navigation_button_frame, text="close program", font=("", 18), height=2, ).grid(row=0, column=4, sticky=customtkinter.W + customtkinter.E)
+        self.close_program_button = customtkinter.CTkButton(self.navigation_button_frame, text="\nClose program\n", font=("roboto", 20), height=0, ).grid(row=0, column=4, sticky=customtkinter.W + customtkinter.E,)
 
         self.navigation_button_frame.pack(fill="x", )
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+    ## start menu content ##
 
 ##        ##------------------------------------------------------------------------------------------------------------
 
@@ -66,17 +102,73 @@ class my_gui:
         self.event_menu_frame_navigation.columnconfigure(3, weight=1, )
         self.event_menu_frame_navigation.columnconfigure(4, weight=1, )
 
-        self.display_scoreboard_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="display scoreboard", font=("", 18), height=2, ).grid(row=0, column=0, sticky=customtkinter.W + customtkinter.E)
+        self.display_scoreboard_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="\nDisplay scoreboard\n", font=("roboto", 20), height=0, command=self.switch_to_display_scoreboard_frame,).grid(row=0, column=0, sticky=customtkinter.W + customtkinter.E)
 
-        self.edit_teams_and_individuals_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="edit teams and individuals", font=("", 18), height=2, ).grid(row=0, column=1, sticky=customtkinter.W + customtkinter.E)
+        self.edit_teams_and_individuals_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="\nEdit teams and individuals\n", font=("roboto", 20), height=2, ).grid(row=0, column=1, sticky=customtkinter.W + customtkinter.E)
 
-        self.start_menu_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="start menu", font=("", 18), height=2, command=self.switch_to_start_menu_frame).grid(row=0, column=2, sticky=customtkinter.W + customtkinter.E)
+        self.start_menu_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="\nStart menu\n", font=("roboto", 20), height=0, command=self.switch_to_start_menu_frame).grid(row=0, column=2, sticky=customtkinter.W + customtkinter.E)
 
-        self.event_menu_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="edit events", font=("", 18), height=2, command=self.switch_to_events_menu_frame).grid(row=0, column=3, sticky=customtkinter.W + customtkinter.E)
+        self.event_menu_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="\nEdit events\n", font=("roboto", 20), height=0, command=self.switch_to_events_menu_frame).grid(row=0, column=3, sticky=customtkinter.W + customtkinter.E)
 
-        self.back_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="back", font=("", 18), height=2, ).grid(row=0, column=4, sticky=customtkinter.W + customtkinter.E)
+        self.back_button = customtkinter.CTkButton(self.event_menu_frame_navigation, text="\nBack\n", font=("roboto", 20), height=0, ).grid(row=0, column=4, sticky=customtkinter.W + customtkinter.E)
 
-        self.root.mainloop()
+##        ##------------------------------------------------------------------------------------------------------------
+
+        ## events menu content ##
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+    ## edit teams and individuals navigation ##
+
+        self.teams_and_individuals_frame_navigation = customtkinter.CTkFrame(self.root, )
+
+        self.teams_and_individuals_frame_navigation.columnconfigure(0, weight=1, )
+        self.teams_and_individuals_frame_navigation.columnconfigure(1, weight=1, )
+        self.teams_and_individuals_frame_navigation.columnconfigure(2, weight=1, )
+        self.teams_and_individuals_frame_navigation.columnconfigure(3, weight=1, )
+        self.teams_and_individuals_frame_navigation.columnconfigure(4, weight=1, )
+
+        self.display_scoreboard_button = customtkinter.CTkButton(self.teams_and_individuals_frame_navigation, text="\nDisplay scoreboard\n", font=("roboto", 20), height=0, command=self.switch_to_display_scoreboard_frame,).grid(row=0, column=0, sticky=customtkinter.W + customtkinter.E)
+
+        self.edit_teams_and_individuals_button = customtkinter.CTkButton(self.teams_and_individuals_frame_navigation, text="\nEdit teams and individuals\n", font=("roboto", 20), height=0, command=self.switch_to_edit_teams_and_individuals_frame, ).grid(row=0, column=1, sticky=customtkinter.W + customtkinter.E)
+
+        self.start_menu_button = customtkinter.CTkButton(self.teams_and_individuals_frame_navigation, text="\nStart menu\n", font=("roboto", 20), height=0, command=self.switch_to_start_menu_frame).grid(row=0, column=2, sticky=customtkinter.W + customtkinter.E)
+
+        self.event_menu_button = customtkinter.CTkButton(self.teams_and_individuals_frame_navigation, text="\nEdit events\n", font=("roboto", 20), height=0, command=self.switch_to_events_menu_frame).grid(row=0, column=3, sticky=customtkinter.W + customtkinter.E)
+
+        self.back_button = customtkinter.CTkButton(self.teams_and_individuals_frame_navigation, text="\nBack\n", font=("roboto", 20), height=0, ).grid(row=0, column=4, sticky=customtkinter.W + customtkinter.E)
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+    ## edit teams and individuals content ##
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+    ## display scoreboard navigation ##
+
+        self.display_scoreboard_frame_navigation = customtkinter.CTkFrame(self.root, )
+
+        self.display_scoreboard_frame_navigation.columnconfigure(0, weight=1, )
+        self.display_scoreboard_frame_navigation.columnconfigure(1, weight=1, )
+        self.display_scoreboard_frame_navigation.columnconfigure(2, weight=1, )
+        self.display_scoreboard_frame_navigation.columnconfigure(3, weight=1, )
+        self.display_scoreboard_frame_navigation.columnconfigure(4, weight=1, )
+
+        self.display_scoreboard_button = customtkinter.CTkButton(self.display_scoreboard_frame_navigation, text="\nDisplay scoreboard\n", font=("roboto", 20), height=0, ).grid(row=0, column=0, sticky=customtkinter.W + customtkinter.E)
+
+        self.edit_teams_and_individuals_button = customtkinter.CTkButton(self.display_scoreboard_frame_navigation, text="\nEdit teams and individuals\n", font=("roboto", 20), height=0, command=self.switch_to_edit_teams_and_individuals_frame,).grid(row=0, column=1, sticky=customtkinter.W + customtkinter.E)
+
+        self.start_menu_button = customtkinter.CTkButton(self.display_scoreboard_frame_navigation, text="\nStart menu\n", font=("roboto", 20), height=0, command=self.switch_to_start_menu_frame).grid(row=0, column=2, sticky=customtkinter.W + customtkinter.E)
+
+        self.event_menu_button = customtkinter.CTkButton(self.display_scoreboard_frame_navigation, text="\nEdit events\n", font=("roboto", 20), height=0, command=self.switch_to_events_menu_frame).grid(row=0, column=3, sticky=customtkinter.W + customtkinter.E)
+
+        self.back_button = customtkinter.CTkButton(self.display_scoreboard_frame_navigation, text="\nBack\n", font=("roboto", 20), height=0, ).grid(row=0, column=4, sticky=customtkinter.W + customtkinter.E)
+
+##        ##------------------------------------------------------------------------------------------------------------
+
+    ## display scoreboard content ##
+
+##        ##------------------------------------------------------------------------------------------------------------
 
         self.root.mainloop()
 
